f575882f28efd40fed9b9106002bc2bad4a27eb6
Daniel Mauser <dmauser@hotmail.com>
2021-03-01 07:51:23 -0600

    Update

--- connect-us-east-spokes.sh
+++ connect-us-east-spokes.sh
@@ -1,3 +1,5 @@
+
+az network vhub show --name microhack-useast-hub --resource-group vwan-microhack-hub-rg
 spoke3id=$(az network vnet show -g vwan-microhack-spoke-rg --name spoke-3-vnet --query "id" --output tsv)
 spoke4id=$(az network vnet show -g vwan-microhack-spoke-rg --name spoke-4-vnet --query "id" --output tsv)
 az network vhub connection create --name spoke-3-useast --resource-group vwan-microhack-hub-rg --vhub-name microhack-useast-hub --remote-vnet $spoke3id --labels default
